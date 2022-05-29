45ee00717b5859c85fbc60253bb32190e661c822
Upekshe <upekshejay@gmail.com>
2022-05-29 12:51:01 +0530

    include providing custom parameters
    Signed-off-by: Upekshe <upekshejay@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 
 export namespace ApiLambda {
 
-    export function wrap<T, R>(handler: (input: T) => Promise<R>, options?: Partial<WrapOptions>)
+    export function wrap<T, U, R>(handler: (input: Input<T, U>) => Promise<R>, options?: Partial<WrapOptions<U>>)
         : (event: APIGatewayProxyEvent, context: Context) => Promise<APIGatewayProxyResult> {
         return async function (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
             try {
-
-                const input: T = JSON.parse(event.body!);
+                const input: Input<T, U> = {
+                    body: event.body ? JSON.parse(event.body!) : undefined,
+                    headers: event.headers,
+                    queryParameters: event.queryStringParameters,
+                    pathParameters: event.pathParameters,
+                    customParameters: options?.customParameters
+                };
                 const value: R = await handler(input);
                 return Response.get200(value);
             } catch (error) {
@@ -17,7 +22,20 @@ export namespace ApiLambda {
         }
     }
 
-    export interface WrapOptions {
+    export interface Input<T, U> {
+        body?: T, // for GET and DELETE requests there is no body
+        headers?: Record<string, string | undefined>;
+        queryParameters?: Record<string, string | undefined> | null;
+        pathParameters?: Record<string, string | undefined> | null;
+        customParameters?: U
+    }
+
+    export interface WrapOptions<T> {
+        customParameters?: T
+    }
+
+    export interface DefaultWrapOptions extends WrapOptions<void> {
+
     }
 
     export namespace Response {
